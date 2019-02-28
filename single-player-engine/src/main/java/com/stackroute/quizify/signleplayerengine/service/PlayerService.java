1f963c01e335248fc1dd38559dd05c04518afda4
madhupriya <madpriy1@in.ibm.com>
2019-02-28 17:11:41 +0530

    single player

--- single-player-engine/src/main/java/com/stackroute/quizify/signleplayerengine/service/PlayerService.java
+++ single-player-engine/src/main/java/com/stackroute/quizify/signleplayerengine/service/PlayerService.java
@@ -2,14 +2,12 @@ package com.stackroute.quizify.signleplayerengine.service;
 
 import com.stackroute.quizify.signleplayerengine.domain.Game;
 import com.stackroute.quizify.signleplayerengine.domain.SinglePlayer;
-import com.stackroute.quizify.signleplayerengine.domain.User;
-import com.stackroute.quizify.signleplayerengine.exception.GamePlayerAlreadyExistsException;
 
 import java.util.Optional;
 
 public interface PlayerService {
 
-    SinglePlayer saveGameHistory(SinglePlayer singlePlayer) throws GamePlayerAlreadyExistsException;
+    SinglePlayer saveGameHistory(SinglePlayer singlePlayer);
 
 
 
